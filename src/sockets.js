54c032575221ea31b1f28e4fa8ad01698989c13e
Andres Cuevas <acg.hermesmusic@gmail.com>
2023-01-16 16:01:18 -0600

    ftr:Agregando envio de correo a servidor

--- src/sockets.js
+++ src/sockets.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import Schedule from "./models/schedule.model";
 
 export default (io) => {
@@ -7,33 +8,52 @@ export default (io) => {
       io.emit("getSche", schedules);
     };
     emitSchedules();
-    socket.on("client:sendSchedule", async (data) => {
-      const user = (await Schedule.findOne({ email: data.email })) || null;
-      if (user === null) {
-        const ScheFind = await Schedule.findById(data.id);
-        if (ScheFind.status === "AVAILABLE") {
-          await Schedule.findByIdAndUpdate(data.id, {
-            user: data.user,
-            email: data.email,
-            status: data.status,
-          });
-          emitSchedules();
-          socket.emit("server:success", {
-            success: true,
-          });
-        } else {
-          const error = {
-            success: false,
-            msg: "Horario no disponible",
-          };
-          socket.emit("server:error", error);
-        }
-      } else {
-        const msg = {
+    socket.on("client:emailExist", async (data) => {
+      const user = await Schedule.findOne({ email: data.email });
+      if (user !== null) {
+        const response = {
           success: false,
-          msg: "Usuario ya registrado",
+          msg: "Usuario registrado",
         };
-        io.emit("server:emailExist", msg);
+        socket.emit("server:emailExistRes", response);
+      } else {
+        const response = {
+          success: true,
+          msg: "",
+        };
+        socket.emit("server:emailExistResOk", response);
+      }
+    });
+    socket.on("client:sendSchedule", async (data) => {
+      const ScheFind = await Schedule.findById(data.id);
+      if (ScheFind.status === "AVAILABLE") {
+        axios
+          .post(
+            "https://back-wguitar-production.up.railway.app/api/v1/user/sendMailDJ",
+            {
+              name: data.user,
+              email: data.email,
+              horario: data.horario,
+            }
+          )
+          .then((res) => console.log(res.data))
+          .catch((err) => console.log(err));
+        await Schedule.findByIdAndUpdate(data.id, {
+          user: data.user,
+          email: data.email,
+          status: data.status,
+        });
+        emitSchedules();
+        socket.emit("server:success", {
+          success: true,
+          msg: "",
+        });
+      } else {
+        const error = {
+          success: false,
+          msg: "Horario no disponible",
+        };
+        socket.emit("server:error", error);
       }
     });
   });
