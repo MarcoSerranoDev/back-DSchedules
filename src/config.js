097a8192624b21716cc96a4f27001b046743a7ee
Andres Cuevas <acg.hermesmusic@gmail.com>
2023-01-16 16:04:29 -0600

    ftr:Eliminacion de la archivo de configuracion email

--- src/config.js
+++ src/config.js
@@ -4,7 +4,3 @@ config();
 
 export const MONGODB__URL = process.env.MONGODB__URL;
 export const PORT = process.env.PORT || 4000;
-export const NODEMAILER_HOST = process.env.NODEMAILER_HOST;
-export const NODEMAILER_PASS = process.env.NODEMAILER_PASS;
-export const NODEMAILER_PORT = process.env.NODEMAILER_PORT;
-export const NODEMAILER_USER = process.env.NODEMAILER_USER;
